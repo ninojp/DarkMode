48a421a88b0dc75c3047b83fcd802fad408cb37d
Edenilson João Pereira <52428137+ninojp@users.noreply.github.com>
2023-12-02 22:53:33 -0300

    Pronto (feito via copia do original)

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// const imgLua = 'https://caelum-online-public.s3.amazonaws.com/1369-alura-mais-dark-mode/lua.png';
+// const imgSol = 'https://caelum-online-public.s3.amazonaws.com/1369-alura-mais-dark-mode/sol.png';
+
+
+document.getElementById('btn').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+})
